c276d4ae6ec87d0c649d9e291e6e9953dfcda4e2
tomlip <t671l197@ku.edu>
2020-06-09 18:54:18 -0500

    Update api_keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "1f9002f08056b648e1057b166d5b65f8"
+weather_api_key = "YOUR CODE HERE!"
 
 # Google API Key
-g_key = "AIzaSyAhmhyNHHi9VmX1StRwky4tssbQgmJQc9w"
+g_key = "YOUR CODE HERE!"
